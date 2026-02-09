fc8d524ef587f6e6a5b8a29918b48ecbf6b457cf
Chipes <carlos1227a@outlook.com>
2026-02-08 23:46:58 -0600

    docs: :memo: Use of Enums

--- backend/api/routes/drivers.py
+++ backend/api/routes/drivers.py
@@ -1,28 +1,29 @@
-from fastapi import APIRouter, Depends
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 # local imports
 from app.dependencies.db import get_db
-from app.models.user import User
+from app.models.user import User, UserRole
 from app.models.delivery import Delivery
 
 router = APIRouter(prefix="/drivers", tags=["Drivers"])
 
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK)
 def get_drivers(db: Session = Depends(get_db)):
-    drivers = db.query(User).filter(User.role == "DRIVER").all()
+    drivers = db.query(User).filter(User.role == UserRole.DRIVER.value).all()
     return {"message": "List of drivers", "data": drivers}
 
-@router.get("/{driver_id}")
-def get_driver(driver_id: int, db: Session = Depends(get_db)):
+@router.get("/{driver_id}", status_code=status.HTTP_200_OK)
+def get_driver(driver_id: UUID, db: Session = Depends(get_db)):
     driver = db.query(User).filter(User.id == driver_id).first()
     if not driver:
-        return {"message": f"Driver with ID {driver_id} not found", "data": None}
+        raise HTTPException(status_code=404, detail=f"Driver with ID {driver_id} not found")
     return {"message": f"Details of driver {driver_id}", "data": driver}
 
 @router.get("/{driver_id}/deliveries")
-def get_driver_deliveries(driver_id: int, db: Session = Depends(get_db)):
+def get_driver_deliveries(driver_id: UUID, db: Session = Depends(get_db)):
     driver = db.query(User).filter(User.id == driver_id).first()
     if not driver:
-        return {"message": f"Driver with ID {driver_id} not found", "data": None}
+        raise HTTPException(status_code=404, detail=f"Driver with ID {driver_id} not found")
     deliveries = db.query(Delivery).filter(Delivery.assigned_to == driver_id).all()
     return {"message": f"Deliveries assigned to driver {driver_id}", "data": deliveries}
