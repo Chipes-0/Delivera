d3f5258af9719952a78a3509c0f354f95764ad80
Chipes <carlos1227a@outlook.com>
2026-02-08 23:22:23 -0600

    docs: :fire: Rename trip -> delivery

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
-
+# local imports
 from .routes.health import router as health_router
 from .routes.drivers import router as drivers_router
-from .routes.trips import router as trips_router
+from .routes.delivery import router as trips_router
 from .routes.expenses import router as expenses_router
 from .routes.users import router as users_router
-
 
 api_router = APIRouter()
 
